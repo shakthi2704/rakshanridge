c1caef013638b7c5781ae4c92438c6aa97136f74
shakthi <shakthi2704@gmail.com>
2026-07-29 18:29:03 +0530

    refactor: remove property and about pages

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,26 +1,9 @@
-import BrandStory from "@/components/home/BrandStory";
-import Experiences from "@/components/home/Experiences";
-import FeaturedProperties from "@/components/home/FeaturedProperties";
-import Hero from "@/components/home/Hero";
-import HospitalityConsulting from "@/components/home/HospitalityConsulting";
-import MainCta from "@/components/ui/MainCta";
-import SpecialOffers from "@/components/home/SpecialOffers";
-import Testimonials from "@/components/home/Testimonials";
-import WhyChooseUs from "@/components/home/WhyChooseUs";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <main className="flex flex-1 flex-col">
-      <Hero />
-      <BrandStory />
-      <FeaturedProperties />
-      <Experiences />
-      <WhyChooseUs />
-      <Testimonials />
-      <SpecialOffers />
-      <HospitalityConsulting />
-      <MainCta />
+
     </main>
   );
 }
